5d7219a2506e4f04c819cb296cf7b900c6d17a58
mperez0207 <mperez0207@gmail.com>
2019-06-26 20:54:31 -0400

    Final Project Completed

--- static/js/app.js
+++ static/js/app.js
@@ -8,17 +8,88 @@ function populate_table()
             Object.entries(row).forEach(function(datum) {
                  tbody.append("td").text(datum[1]);
             });
-            // // Append new row to table so we can enter the next UFO sighting data
-            // tbody.append("tr").attr("class", "data_row");
-
-            // // Loop through each datum for sighting and call function
-            // Object.entries(ufoData).forEach(function([key, value]) {
-            //         // For each datum append a new cell to the table and fill the cell with datum
-            //         tbody.append("td").text(value);
-            // });
         });
     });
 }
 
-// When webpage is first loaded, populate the table 
+function results_bar_graph()
+{
+    d3.csv("/Prediction Results.csv", function(data) {
+
+        var precision = {
+            x: [],
+            y: [],
+            text: [],
+            name: "",
+            type: "bar"
+        };
+
+        var recall = {
+            x: [],
+            y: [],
+            text: [],
+            name: "",
+            type: "bar"
+        };
+
+        data.forEach(function(row) {
+            var rent_category = row["Rent Category"];
+
+            precision["x"].push(rent_category);
+            recall["x"].push(rent_category);
+
+            precision["y"].push(row["precision"]);
+            recall["y"].push(row["recall"]);
+
+            precision["name"] = "Precision"
+            recall["name"] = "Recall"
+
+            var support = row["support"];
+
+            var hover_text = support + " neighborhoods were " + rent_category;
+            precision["text"].push(hover_text);
+            recall["text"].push(hover_text);
+        });
+
+        var data = [precision, recall];
+            
+        var layout = {
+            barmode: 'group',
+            title: {
+                text:'Precision and Recall For Each Rent Category',
+                font: {
+                  family: 'Courier New, monospace',
+                  size: 24
+                },
+                xref: 'paper',
+                x: 0.05,
+              },
+              xaxis: {
+                title: {
+                  text: 'Rent Category',
+                  font: {
+                    family: 'Courier New, monospace',
+                    size: 18,
+                    color: '#7f7f7f'
+                  }
+                },
+              },
+              yaxis: {
+                title: {
+                  text: 'Percentage',
+                  font: {
+                    family: 'Courier New, monospace',
+                    size: 18,
+                    color: '#7f7f7f'
+                  }
+                }
+              }
+        };
+    
+        Plotly.newPlot('results_graph', data, layout);
+    });
+}
+
+// When webpage is first loaded, populate the table and display the results bar graph
 populate_table();
+results_bar_graph();
